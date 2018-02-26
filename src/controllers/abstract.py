c0bf5a85fd2f1274ab4bf3d11876aa1f6e97c6b2
kYs <croy@student.42.fr>
2018-02-26 22:35:27 +0100

    fix errors on app startup

--- src/controllers/abstract.py
+++ src/controllers/abstract.py
@@ -1,16 +1,15 @@
 import json
-from bson import json_util
 from flask import request, jsonify, Response, Flask
 
 class AbstractController:
-	def __init__(self, injector, prefixUrl, nameController)
+	def __init__(self, injector, prefixUrl, nameController):
 		self.app = injector.resolve("Flask")
 
 		self.app.add_url_rule(prefixUrl, nameController + ".get", self.get, methods=["GET"])
-		self.app.add_url_rule(prefixUrl + "</string:identifier>", nameController + ".getbyid", self.getById, methods=["GET"])
+		self.app.add_url_rule(prefixUrl + "/<string:identifier>", nameController + ".getbyid", self.getById, methods=["GET"])
 
 	def buildResponse(self, data):
-		serialized = json_util.dumps({
+		serialized = json.dumps({
 			"success": True,
 			"data": data
 		})
@@ -18,7 +17,7 @@ class AbstractController:
 		return Response(serialized, mimetype="application/json")
 
 	def buildError(self, error):
-		serialized = json_util.dumps({
+		serialized = json.dumps({
 			"success": False,
 			"detail": error
 		})
--- src/controllers/logs.py
+++ src/controllers/logs.py
@@ -6,10 +6,13 @@ import docker
 class LogsController(AbstractController):
 	def __init__(self, injector):
 		self.dockerClient = docker.from_env()
-		nameController str(type(self)).lower()
+		nameController = str(type(self)).lower()
 		prefixUrl = "/logs"
 		super(LogsController, self).__init__(injector, prefixUrl, nameController)
 
 	def getById(self, identifier):
 		container = self.dockerClient.containers.get(identifier)
 		return super(ContainersController, self).buildResponse(container.logs())
+
+	def get(self):
+		return false
