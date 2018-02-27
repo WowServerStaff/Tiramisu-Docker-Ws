05ba0dacc2313dafdca55203e67c13dcf669a388
NCharvy <nathan.charvy@gmail.com>
2018-02-27 12:02:52 +0100

    Update provider.py

--- src/services/provider.py
+++ src/services/provider.py
@@ -1,8 +1,25 @@
+"""
+Class used as dependency injector.
+
+Allows the app to register and retrieve any class.
+"""
+
 import inspect
 
 class Provider:
 	modules = {}
 
+	"""
+	Registering method.
+	
+	Param1: the call name one the class is registered
+	Param2: the class itself
+	
+	The function test if the instance is a class, and invoke its
+	constructor in this case (the provider is injected inside).
+	In the other case, we admit it's an instanciated object.
+	We put it inside the modules object with the proper name
+	"""
 	def register(self, name, instance):
 		if(inspect.isclass(instance)):
 			resource = instance(self)
@@ -10,6 +27,14 @@ class Provider:
 			resource = instance
 		self.modules[name] = resource
 
+	"""
+	Resolving method.
+	
+	Param: the name of the module we try to invoke
+	
+	If the name correspond to one of the modules, we return it.
+	In the other case, raise an exception.
+	"""
 	def resolve(self, name):
 		if(name in self.modules):
 			return self.modules[name]
